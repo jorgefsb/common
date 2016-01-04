832f53a8fc8e41b234f53510fc9ae27614c73029
Connor Peet <connor@peet.io>
2016-01-05 08:30:53 +1000

    Allow level ==> xp conversion

--- spec/LevelSpec.js
+++ spec/LevelSpec.js
@@ -1,27 +1,38 @@
 var xp = require('../xp');
 describe('xp', function () {
+    var boundary = [
+        { level: 0, to: 5 },
+        { level: 1, to: 14 },
+        { level: 2, to: 27 },
+        { level: 3, to: 45 },
+        { level: 4, to: 66 },
+        { level: 5, to: 92 },
+        { level: 6, to: 122 },
+        { level: 7, to: 156 },
+        { level: 8, to: 195 },
+        { level: 9, to: 237 },
+    ];
+
+    beforeEach(function () {
+        xp.flushCache();
+    });
+
     it('generates levels correctly', function () {
-        var boundary = [
-            { level: 0, to: 6 },
-            { level: 1, to: 15 },
-            { level: 2, to: 28 },
-            { level: 3, to: 46 },
-            { level: 4, to: 67 },
-            { level: 5, to: 93 },
-            { level: 6, to: 123 },
-            { level: 7, to: 157 },
-            { level: 8, to: 195 },
-            { level: 9, to: 238 },
-        ];
         for (var k = 0; k < 1; k++) {
             var bound = 0;
-            for (var i = 0; i < 100; i++) {
+            for (var i = 0; i < 200; i++) {
                 if (i >= boundary[bound].to) {
                     bound++;
                 }
 
                 expect(xp.getLevel(i)).toBe(boundary[bound].level);
             }
+        }
+    });
+
+    it('gets xp at level correctly', function () {
+        for (var i = 1; i < boundary.length; i++) {
+            expect(xp.getXp(boundary[i].level)).toBe(boundary[i - 1].to);
         }
     });
 });
--- xp/index.js
+++ xp/index.js
@@ -1,35 +1,69 @@
+function Calculator() {
+    this.flushCache();
+}
+
 /**
- * Converts a number of experience points to the level that the user is at.
- * See the table the beam backend docs for the actual values.
+ * Clears the calculator's cache. Mainly for debugging.
+ */
+Calculator.prototype.flushCache = function () {
+    this.cache = [{ xp: 0, level: 0 }];
+};
+
+/**
+ * Adds a new level to the cache.
+ */
+Calculator.prototype._buildLevel = function () {
+    var last = this._lastLevel();
+    var level = last.level + 1;
+
+    var xp = 4 * level + Math.pow(1.5, level / 4) + last.xp;
+    this.cache.push({
+        level: level,
+        xpFloor: Math.floor(xp),
+        xp: xp,
+    });
+};
+
+/**
+ * Returns the last level stored in the cache.
+ * @return {Object}
+ */
+Calculator.prototype._lastLevel = function () {
+    return this.cache[this.cache.length - 1];
+};
+
+/**
+ * Returns the level that the user with the given XP is at.
  * @param  {Number} xp
  * @return {Number}
  */
-exports.getLevel = (function () {
-    // Computing the level curve is fairly expensive, so I added this cache
-    // for when this gets used on the backend or abused on the frontend.
-    var cache = [{ xp: 0, level: 0 }];
+Calculator.prototype.getLevel = function (xp) {
+    while (this._lastLevel().xp < xp) {
+        this._buildLevel();
+    }
 
-    return function (xp) {
-        for (var i = 1; i < cache.length; i++) {
-            if (cache[i].xp > xp) {
-                return cache[i - 1].level;
-            }
+    var cache = this.cache;
+    for (var i = cache.length - 1; i >= 0; i--) {
+        if (cache[i].xpFloor <= xp) {
+            return cache[i].level;
         }
+    }
 
-        // if we got to here, expand the cache
-        var last = cache[cache.length - 1];
-        while (last.xp <= xp) {
-            var level = last.level + 1;
-            last = {
-                level: level,
-                xp: 4 * level + Math.pow(1.5, level / 4) + last.xp
-            };
+    return 0;
+};
 
-            cache.push(last);
-        }
+/**
+ * Returns the amount of XP necessary to reach the given level.
+ * @param  {Number} level
+ * @return {Number}
+ */
+Calculator.prototype.getXp = function (level) {
+    var cache = this.cache;
+    while (cache.length <= level) {
+        this._buildLevel();
+    }
 
-        // Return the second to last level, since we just generated
-        // up to the level after the user's current one.
-        return cache[cache.length - 2].level;
-    };
-})();
+    return cache[level].xpFloor;
+};
+
+module.exports = new Calculator();
