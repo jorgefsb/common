6f1ecb46c182b8f9e8686317017735ec997abfd9
Connor Peet <connor@peet.io>
2016-02-01 20:47:50 -0500

    Give the global mods suspension permission

--- roles/globalmod.js
+++ roles/globalmod.js
@@ -4,6 +4,9 @@ function GlobalModRole () {
     this.name = 'GlobalMod';
     this.level = 60;
     this.inherits = ['Mod'];
+    this.permissions = [
+        'channel:suspend',
+    ];
 
     Role.call(this);
 }
